a88b826886897b7f1afaf5ec3ccfb13dbe28ea37
vinaywadhwa <vinaywadhwa@gmail.com>
2015-01-13 18:08:42 +0530

    Committing ported code

---
+++ src/com/vwap/barcodescanner/Result.java
@@ -0,0 +1,21 @@
+package com.vwap.barcodescanner;
+public class Result {
+    private String mContents;
+    private BarcodeFormat mBarcodeFormat;
+
+    public void setContents(String contents) {
+        mContents = contents;
+    }
+
+    public void setBarcodeFormat(BarcodeFormat format) {
+        mBarcodeFormat = format;
+    }
+
+    public BarcodeFormat getBarcodeFormat() {
+        return mBarcodeFormat;
+    }
+
+    public String getContents() {
+        return mContents;
+    }
+}
